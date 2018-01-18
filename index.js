4781780d1c4497a72b5b3325cb76c87170634c6d
Kevin Nguy <kevin@macbaby.local>
2018-01-18 03:25:03 -0800

    Create student course and course grade object

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const Papa = require('papaparse');
 const fs = require('fs');
+const isEqual = require('lodash/isEqual');
 
+// read and write file and directory
 const file = fs.readFileSync('./seniors.csv', 'utf8');
+if (!fs.existsSync(`${__dirname}/students/`)){
+    fs.mkdirSync(`${__dirname}/students/`);
+}
 
+// read csv
 const { data, errors } = Papa.parse(
   file,
   { dynamicTyping: true, header: true },
 );
 
+// create students array
 const students = data.reduce((acc, value, currentIndex) => {
   const studentName = value['Name'];
-  const currentStudent = acc.filter(student => student.name === studentName)[0];
+  const student = acc.filter(
+    selectedStudent => selectedStudent.name === studentName
+  )[0];
   const course = {
     id: value['CourseID'],
     title: value['CourseTitle'],
-    mark: value['Mark'],
+  };
+  const grade = {
+    value: value['Mark'],
     creditAttempted: value['CreditAttempted'],
     creditCompleted: value['CreditCompleted'],
     studentGrade: value['Grade'],
     month: value['Calendar Month'],
     year: value['Calendar Year'],
-  };
+  }
 
-  if (!currentStudent) {
+  if (
+    !grade.value || // no grade because class is in progress
+    grade.creditAttempted === 0 // zero credit is not needed
+  ) {
+    return acc;
+  }
+
+  if (!student) {
+    // create student object and store it
+    course.grades = [grade];
     acc.push({
       name: studentName,
       courses: [course],
@@ -31,24 +51,46 @@ const students = data.reduce((acc, value, currentIndex) => {
     return acc;
   }
 
-  // TODO: remove rows with no grade because class is in progress
-  // TODO: remove rows with zero credit attempted
+  let duplicateCourse = false;
+  student.courses.forEach(selectedCourse => {
+    if (selectedCourse.id === course.id) {
+      duplicateCourse = true;
 
-  currentStudent.courses.push(course);
+      let duplicateGrade = false;
+      selectedCourse.grades.forEach(selectedGrade => {
+        if (isEqual(selectedGrade, grade)) {
+          duplicateGrade = true;
+          return false;
+        }
+      });
+
+      if (!duplicateGrade) {
+        selectedCourse.grades.push(grade);
+      }
+    }
+  });
+
+  if (!duplicateCourse) {
+    course.grades = [grade];
+    student.courses.push(course);
+  }
+
   return acc;
 }, []);
 
-if (!fs.existsSync(`${__dirname}/students/`)){
-    fs.mkdirSync(`${__dirname}/students/`);
-}
-
+// create student txt files
 students.forEach(student => {
   const { name, courses } = student;
 
+  // sort student's courses
+
+
+
+  // write file
   let fileString = `${name}\n\n`;
-  // const ninthGradeRows = rows.filter(row => row['Grade'] === 9);
   courses.forEach(course => {
-    fileString = `${fileString}\n${course.title}${course.mark.padStart(20 - course.title.length)}  ${course.creditCompleted}/${course.creditAttempted}`
+    // fileString = `${fileString}${course.title}${course.mark.padStart(20 - course.title.length)}  ${course.creditCompleted}/${course.creditAttempted}`
+    fileString = `${fileString}\n\n${JSON.stringify(course, undefined, 2)}`;
   });
 
   const fileName = `${name.replace(/ /g,'_').replace(/[.,]/g,'')}.txt`;
