59cf0a6eec0d590abffbf94d9094ddaf5609bb5d
Kevin Nguy <kevin@macbaby.local>
2018-01-18 02:32:45 -0800

    Extract particular values from csv row for each course

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ const { data, errors } = Papa.parse(
   { dynamicTyping: true, header: true },
 );
 
-const students = data.reduce((
-  acc,
-  value,
-  currentIndex,
-  // array
-) => {
-  if (currentIndex === 1) { // currentIndex starts at 1 without initialValue
-    return [{
-      name: acc['Name'],
-      rows: [acc],
-    }];
-  }
+const students = data.reduce((acc, value, currentIndex) => {
+  const studentName = value['Name'];
+  const currentStudent = acc.filter(student => student.name === studentName)[0];
+  const course = {
+    id: value['CourseID'],
+    title: value['CourseTitle'],
+    mark: value['Mark'],
+    creditAttempted: value['CreditAttempted'],
+    creditCompleted: value['CreditCompleted'],
+    studentGrade: value['Grade'],
+    month: value['Calendar Month'],
+    year: value['Calendar Year'],
+  };
 
-  const lastStudent = acc[acc.length - 1];
-
-  if (lastStudent.name !== value['Name']) {
+  if (!currentStudent) {
     acc.push({
-      name: value['Name'],
-      rows: [value],
+      name: studentName,
+      courses: [course],
     });
 
     return acc;
@@ -35,21 +34,21 @@ const students = data.reduce((
   // TODO: remove rows with no grade because class is in progress
   // TODO: remove rows with zero credit attempted
 
-  lastStudent.rows.push(value);
+  currentStudent.courses.push(course);
   return acc;
-});
+}, []);
 
 if (!fs.existsSync(`${__dirname}/students/`)){
     fs.mkdirSync(`${__dirname}/students/`);
 }
 
 students.forEach(student => {
-  const { name, rows } = student;
+  const { name, courses } = student;
 
-  let fileString = `${name}\n\n9th Grade`;
+  let fileString = `${name}\n\n`;
   // const ninthGradeRows = rows.filter(row => row['Grade'] === 9);
-  rows.forEach(row => {
-    fileString = `${fileString}\n${row['Course Title']} - Grade: ${row['Mark']} Credit: ${row['GenericField1']}/${row['GenericField2']}`
+  courses.forEach(course => {
+    fileString = `${fileString}\n${course.title}${course.mark.padStart(20 - course.title.length)}  ${course.creditCompleted}/${course.creditAttempted}`
   });
 
   const fileName = `${name.replace(/ /g,'_').replace(/[.,]/g,'')}.txt`;
