5b15e0d26a5f2e579853335bea9f52571ea4872a
Kevin Nguy <kevin@macbaby.local>
2018-01-18 12:57:56 -0800

    Calculate student's course category total credits

--- index.js
+++ index.js
@@ -2,61 +2,68 @@ const Papa = require('papaparse');
 const fs = require('fs');
 const isEqual = require('lodash/isEqual');
 
+// make students directory
+if (!fs.existsSync(`${__dirname}/students/`)){
+    fs.mkdirSync(`${__dirname}/students/`);
+}
+
 // read and write file and directory
 const file = fs.readFileSync('./seniors.csv', 'utf8');
+const ignoreCategoryCodes = fs.readFileSync('./codes/ignore.txt', 'utf8').split('\n');
 
 const courseCategories = {
   english: {
     codes: fs.readFileSync('./codes/english.txt', 'utf8').split('\n'),
     creditsTotal: 40,
+    value: 'English',
   },
   history: {
     codes: fs.readFileSync('./codes/history_ss.txt', 'utf8').split('\n'),
     creditsTotal: 30,
+    value: 'History/Social Sciences',
   },
   math: {
     codes: fs.readFileSync('./codes/math.txt', 'utf8').split('\n'),
     creditsTotal: 30,
+    value: 'Mathematics',
   },
   science: {
     codes: fs.readFileSync('./codes/science.txt', 'utf8').split('\n'),
     creditsTotal: 20,
+    value: 'Science',
   },
   language: {
     codes: fs.readFileSync('./codes/language.txt', 'utf8').split('\n'),
     creditsTotal: 20,
+    value: 'World Language',
   },
   visual: {
     codes: fs.readFileSync('./codes/visual_pa.txt', 'utf8').split('\n'),
     creditsTotal: 10,
+    value: 'Visual and Performing Arts',
   },
   pe: {
     codes: fs.readFileSync('./codes/pe.txt', 'utf8').split('\n'),
     creditsTotal: 20,
+    value: 'Physical Education',
   },
   electives: {
     codes: fs.readFileSync('./codes/electives.txt', 'utf8').split('\n'),
     creditsTotal: 50,
+    value: 'Electives',
   },
   collegeCareer: {
     codes: fs.readFileSync('./codes/college_career.txt', 'utf8').split('\n'),
     creditsTotal: 5,
+    value: 'College Career',
   },
   health: {
     codes: fs.readFileSync('./codes/health.txt', 'utf8').split('\n'),
     creditsTotal: 5,
-  },
-  ignore: {
-    codes: fs.readFileSync('./codes/ignore.txt', 'utf8').split('\n'),
-    creditsTotal: 0,
+    value: 'Health Education',
   },
 };
-
-console.log(Object.keys(courseCategories));
-
-if (!fs.existsSync(`${__dirname}/students/`)){
-    fs.mkdirSync(`${__dirname}/students/`);
-}
+const courseCategoriesKeys = Object.keys(courseCategories);
 
 // read csv
 const { data, errors } = Papa.parse(
@@ -65,8 +72,6 @@ const { data, errors } = Papa.parse(
 );
 
 const findCourseCategory = id => {
-  const courseCategoriesKeys = Object.keys(courseCategories);
-
   let category = 'N/A';
   courseCategoriesKeys.forEach(selectedCategory => {
     const codes = courseCategories[selectedCategory].codes;
@@ -148,21 +153,17 @@ const students = data.reduce((acc, value, currentIndex) => {
 students.forEach(student => {
   const { name, courses } = student;
 
-  // sort student's courses by name
+  // sort student's courses by name and category
   courses.sort((a, b) => {
     const titleA = a.title.toLowerCase();
     const titleB = b.title.toLowerCase();
-
-    if (titleA < titleB) {
-      return -1;
-    }
-
-    if (titleA > titleB) {
-      return 1;
-    }
-
-    return 0;
-  });
+    return titleA > titleB ? 1 : -1;
+  })
+  // .sort((a, b) => {
+  //   const indexA = courseCategoriesKeys.indexOf(a.category);
+  //   const indexB = courseCategoriesKeys.indexOf(b.category);
+  //   return indexA > indexB ? 1 : -1;
+  // });
 
   // sort course's grades chronologically
   courses.forEach(course => {
@@ -173,12 +174,27 @@ students.forEach(student => {
 
   // write file
   let fileString = `${name}\n\n`;
-  courses.forEach(course => {
-    fileString = `${fileString}\n\n${course.title}`;
 
-    course.grades.forEach(grade => {
-      fileString = `${fileString}\n  ${grade.value}  ${grade.creditCompleted}/${grade.creditAttempted}  ${grade.month < 10 ? `0${grade.month}` : grade.month}/${grade.year}    ${course.category}`;
-    })
+  courseCategoriesKeys.forEach(key => {
+    const category = courseCategories[key];
+    const filteredCourses = courses.filter(course => course.category === key);
+    const creditsCompleted = filteredCourses.reduce((acc, course) => {
+      course.grades.forEach(grade => { acc = acc + grade.creditCompleted; });
+      return acc;
+    }, 0);
+
+    const title = `${category.value}: ${creditsCompleted}/${category.creditsTotal}`;
+    fileString = `${fileString}${title}\n${'='.repeat(title.length)}`;
+
+    filteredCourses.forEach(course => {
+      fileString = `${fileString}\n  ${course.title}`;
+
+      course.grades.forEach(grade => {
+        fileString = `${fileString}\n    ${grade.value}  ${grade.creditCompleted}/${grade.creditAttempted}  ${grade.month < 10 ? `0${grade.month}` : grade.month}/${grade.year}`;
+      })
+    });
+
+    fileString = `${fileString}\n\n\n`
   });
 
   const fileName = `${name.replace(/ /g,'_').replace(/[.,]/g,'')}.txt`;
