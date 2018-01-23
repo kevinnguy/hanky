b23e8ba00eda4aa32b25704a540c457f8f53da92
Kevin Nguy <kevin@macbaby.local>
2018-01-23 13:40:52 -0800

    Fix course id usage

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,92 +1,79 @@
 import Papa from 'papaparse';
-import fs from 'fs';
+import FileSaver from 'file-saver';
+import JSZip from 'jszip';
 import isEqual from 'lodash/isEqual';
 
-// make students directory
-const makeFileDirectory = directoryName => {
-  const path = `${__dirname}/${directoryName}/`;
-  if (!fs.existsSync(path)){
-      fs.mkdirSync(path);
+import courseIDs from '../constants/courseIDs';
+
+const courseCategories = {
+  english: {
+    codes: courseIDs.english,
+    creditsTotal: 40,
+    value: 'English',
+  },
+  history: {
+    codes: courseIDs.history,
+    creditsTotal: 30,
+    value: 'History/Social Sciences',
+  },
+  math: {
+    codes: courseIDs.math,
+    creditsTotal: 30,
+    value: 'Mathematics',
+  },
+  science: {
+    codes: courseIDs.science,
+    creditsTotal: 20,
+    value: 'Science',
+  },
+  language: {
+    codes: courseIDs.language,
+    creditsTotal: 20,
+    value: 'World Language',
+  },
+  visual: {
+    codes: courseIDs.visual,
+    creditsTotal: 10,
+    value: 'Visual and Performing Arts',
+  },
+  pe: {
+    codes: courseIDs.pe,
+    creditsTotal: 20,
+    value: 'Physical Education',
+  },
+  electives: {
+    codes: courseIDs.electives,
+    creditsTotal: 50,
+    value: 'Electives',
+  },
+  collegeCareer: {
+    codes: courseIDs.collegeCareer,
+    creditsTotal: 5,
+    value: 'College Career',
+  },
+  health: {
+    codes: courseIDs.health,
+    creditsTotal: 5,
+    value: 'Health Education',
+  },
+  notCategorized: {
+    codes: [],
+    creditsTotal: 0,
+    value: 'Not Categorized',
   }
-}
-
-const readFile = path => readFile(path, 'utf8')
-
-// read and write file and directory
-// const file = readFile('./seniors.csv');
-// const ignoreCategoryCodes = readFile('./codes/ignore.txt').split('\n');
-
-// const courseCategories = {
-//   english: {
-//     codes: readFile('./codes/english.txt').split('\n'),
-//     creditsTotal: 40,
-//     value: 'English',
-//   },
-//   history: {
-//     codes: readFile('./codes/history_ss.txt').split('\n'),
-//     creditsTotal: 30,
-//     value: 'History/Social Sciences',
-//   },
-//   math: {
-//     codes: readFile('./codes/math.txt').split('\n'),
-//     creditsTotal: 30,
-//     value: 'Mathematics',
-//   },
-//   science: {
-//     codes: readFile('./codes/science.txt').split('\n'),
-//     creditsTotal: 20,
-//     value: 'Science',
-//   },
-//   language: {
-//     codes: readFile('./codes/language.txt').split('\n'),
-//     creditsTotal: 20,
-//     value: 'World Language',
-//   },
-//   visual: {
-//     codes: readFile('./codes/visual_pa.txt').split('\n'),
-//     creditsTotal: 10,
-//     value: 'Visual and Performing Arts',
-//   },
-//   pe: {
-//     codes: readFile('./codes/pe.txt').split('\n'),
-//     creditsTotal: 20,
-//     value: 'Physical Education',
-//   },
-//   electives: {
-//     codes: readFile('./codes/electives.txt').split('\n'),
-//     creditsTotal: 50,
-//     value: 'Electives',
-//   },
-//   collegeCareer: {
-//     codes: readFile('./codes/college_career.txt').split('\n'),
-//     creditsTotal: 5,
-//     value: 'College Career',
-//   },
-//   health: {
-//     codes: readFile('./codes/health.txt').split('\n'),
-//     creditsTotal: 5,
-//     value: 'Health Education',
-//   },
-//   notCategorized: {
-//     codes: [],
-//     creditsTotal: 0,
-//     value: 'Not Categorized',
-//   }
-// };
-// const courseCategoriesKeys = Object.keys(courseCategories);
-
-// read csv
-const csvFromFilePath = path => {
-  const file = readFile(path);
-  return Papa.parse(file, { dynamicTyping: true, header: true });
 };
 
-const findCourseCategory = ({ courseID, categories }) => {
-  const keys = Object.keys(categories);
+const csvFromData = data => Papa.parse(
+  data,
+  { dynamicTyping: true, header: true },
+);
+
+const findCourseCategory = courseID => {
+  const keys = Object.keys(courseCategories);
 
   let categoryType = 'notCategorized';
   keys.forEach(key => {
-    const codes = categories[key].codes;
+    const codes = courseCategories[key].codes;
     if (codes.find(code => code === courseID)) {
       categoryType = key;
       return false;
@@ -162,37 +149,32 @@ const studentsFromCSV = csv => (
   }, [])
 );
 
-const writeStudentsFile = ({
-  students,
-  destinationPath,
-  categories,
-}) => {
-  const categoriesKeys = Object.keys(categories);
-
-  students.forEach(student => {
-    const { name, courses } = student;
-
-    // sort student's courses by name and category
+const graduationRequirementsFromStudents = students => {
+  const categoriesKeys = Object.keys(courseCategories);
+  const sortCourses = courses => {
     courses.sort((a, b) => {
       const titleA = a.title.toLowerCase();
       const titleB = b.title.toLowerCase();
       return titleA > titleB ? 1 : -1;
-    })
+    });
 
-    // sort course's grades chronologically
     courses.forEach(course => {
       course.grades.sort((a, b) => (
         b.year - a.year
       )).sort((a, b) => b.month - a.month);
     });
+  }
+
+  return students.map(student => {
+    const { name, courses } = student;
+    sortCourses(courses);
 
     // write file
-    let fileString = `${name}\n\n`;
-
+    let file = `${name}\n\n`;
     let extraElectiveCredits = [];
 
     categoriesKeys.forEach(key => {
-      const category = categories[key];
+      const category = courseCategories[key];
       const filteredCourses = courses.filter(course => course.category === key);
       let creditsCompleted = filteredCourses.reduce((acc, course) => {
         course.grades.forEach(grade => { acc = acc + grade.creditCompleted; });
@@ -213,49 +195,37 @@ const writeStudentsFile = ({
       }
 
       const title = `${category.value}: ${creditsCompleted}/${category.creditsTotal}`;
-      fileString = `${fileString}${title}\n${'='.repeat(title.length)}`;
+      file = `${file}${title}\n${'='.repeat(title.length)}`;
 
       filteredCourses.forEach(course => {
-        fileString = `${fileString}\n  ${course.title}`;
+        file = `${file}\n  ${course.title}`;
 
         course.grades.forEach(grade => {
-          fileString = `${fileString}\n    ${grade.value}  ${grade.creditCompleted}/${grade.creditAttempted}  ${grade.month < 10 ? `0${grade.month}` : grade.month}/${grade.year}`;
+          file = `${file}\n    ${grade.value}  ${grade.creditCompleted}/${grade.creditAttempted}  ${grade.month < 10 ? `0${grade.month}` : grade.month}/${grade.year}`;
         })
       });
 
       if (key === 'electives' && extraElectiveCredits.length) {
-        fileString = `${fileString}\n  Extra Credits`;
+        file = `${file}\n  Extra Credits`;
         extraElectiveCredits.forEach(object => {
-          fileString = `${fileString}\n    ${object.category.value}  ${object.value}`;
+          file = `${file}\n    ${object.category.value}  ${object.value}`;
         })
       }
 
-      fileString = `${fileString}\n\n\n`
+      file = `${file}\n\n\n`;
     });
 
-    const fileName = `${name.replace(/ /g,'_').replace(/[.,]/g,'')}.txt`;
-    fs.writeFileSync(`${destinationPath}/${fileName}`, fileString);
+    return {
+      courses,
+      file,
+      name,
+    };
   });
 }
 
-const writeCoursesFile = ({ csv, destinationPath }) => {
-  const { data } = csv;
-  if (!data) {
-    console.log('Cannot write courses file');
-    return;
-  }
-
-  const courses = new Set();
-  data.forEach(row => {
-    const courseID = row['CourseID'];
-    courses.add(courseID);
-  })
-
-  fs.writeFileSync(`${destinationPath}/courses.txt`, [...courses].sort().join('\n'));
-}
-
 export {
-  csvFromFilePath,
+  courseCategories,
+  csvFromData,
+  graduationRequirementsFromStudents,
   studentsFromCSV,
-  writeStudentsFile,
 }
